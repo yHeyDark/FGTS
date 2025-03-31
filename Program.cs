db5673b9916102d6ff6ae4eb31cb45a7184cad80
Derik Rocha <122660784+yHeyDark@users.noreply.github.com>
2025-03-31 20:40:06 -0300

    add FGTS

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double sala, fgts;
+
+Console.Write("Digite o Salario: ");
+sala = Convert.ToDouble(Console.ReadLine());
+fgts = sala*0.08;
+Console.WriteLine($"Salario de ...: {sala:N2}");
+Console.Write($"O seu fundo de garantia e de  R$ {fgts:N2}");
